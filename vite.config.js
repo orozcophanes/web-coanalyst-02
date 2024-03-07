5d97af710fd11aaa60f2a56d298e9cb8b41709e7
orozcophanes <orozconez@proton.me>
2024-03-07 06:08:00 -0600

    Update vite.config.js and other changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/web-coanalyst-02/",
+  base: "https://orozcophanes.github.io/web-coanalyst-02/",
   plugins: [vue()],
   resolve: {
     alias: [
